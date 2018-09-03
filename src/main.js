a3190b5b424953597df641a21bfc3cc688c97ae2
Tareq Hossain <xtrinsic96@gmail.com>
2018-09-03 11:14:35 -0400

    update packege version

--- src/main.js
+++ src/main.js
@@ -1,11 +1,3 @@
 import './assets/scss/style.scss';
-var app = require('./controllers/app');
 
-console.log(app.name);
-
-function getAuthor()
-{
-    return "Tareq Hossain"
-}
-
-console.log(getAuthor())
+//do your staff here
