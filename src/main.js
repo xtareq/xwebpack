e9a263db8652d4e0475866fd48c4ca4894c95a6d
Tareq Hossain <xtrinsic96@gmail.com>
2018-09-03 11:05:06 -0400

    mainjs changed

--- src/main.js
+++ src/main.js
@@ -2,3 +2,10 @@ import './assets/scss/style.scss';
 var app = require('./controllers/app');
 
 console.log(app.name);
+
+function getAuthor()
+{
+    return "Tareq Hossain"
+}
+
+console.log(getAuthor())
